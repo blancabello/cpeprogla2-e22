8b2071f5c4fb2a0958cfa3f11cc078e3e2c7a2be
blancabello <mikyleciencia@gmail.com>
2016-09-26 16:12:18 +0800

    Create Hashmat.cpp

---
+++ Hashmat.cpp
@@ -0,0 +1,19 @@
+//Hashmat
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int x;
+	int y;
+	int z;
+	
+	cout<<"Hashmat's army: ";
+	cin>> x;
+	cout<<"opponent's army: ";
+	cin>> y;
+	
+	z = abs (x-y);
+	cout<<z;
+}

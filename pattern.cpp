65ed2984315b742e65fbd614a2e8c74613842221
blancabello <mikyleciencia@gmail.com>
2016-10-28 17:44:07 +0800

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -1,17 +1,18 @@
-include <iostream>
-include <fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-int main(){
-  int arr[]={1,2,4,3};
-  int x=0;
-  int a, b ,i;
-  ifstream read("input.txt");
-  read >> a >> b;
-  for(i=a;i<=b;i++){
-  cout << i-- <<" ";
-       i+=arr[(x%4)];  
-  x++;
-}
 
-return 0;
+int main(){
+    int arr[]={1,2,4,3};
+    int x=0;
+	  int a, b ,i;
+	
+    ifstream f("input.txt");
+    f >> a >> b;
+    for(i=a;i<=b;i++){
+      cout << i-- <<" ";
+           i+=arr[(x%4)];  
+      x++;
+    }
+    return 0;
 }
